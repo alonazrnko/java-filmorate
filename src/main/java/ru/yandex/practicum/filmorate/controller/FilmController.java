edb740fd2406abd55e3f54004cbcadaffa005a6d
alonazrnko <alonazrnko@gmail.com>
2025-12-11 13:50:24 +0300

    first commit

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,74 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
 @RestController
+@RequestMapping("/films")
 public class FilmController {
+
+    private final List<Film> films = new ArrayList<>();
+    private int nextId = 1;
+
+    private static final LocalDate CINEMA_BIRTHDAY = LocalDate.of(1895, 12, 28);
+
+    private void validateDate(Film film) {
+        if (film.getReleaseDate().isBefore(CINEMA_BIRTHDAY)) {
+            throw new ValidationException("Дата релиза не может быть раньше 28.12.1895");
+        }
+    }
+
+    @PostMapping
+    public Film addFilm(@Valid @RequestBody Film film) {
+        validate(film);
+        validateDate(film);
+        film.setId(nextId++);
+        films.add(film);
+        log.info("Добавлен новый фильм: {}", film);
+        return film;
+    }
+
+    @PutMapping
+    public Film updateFilm(@Valid @RequestBody Film film) {
+        validate(film);
+        validateDate(film);
+        for (int i = 0; i < films.size(); i++) {
+            if (films.get(i).getId() == film.getId()) {
+                films.set(i, film);
+                log.info("Обновлен фильм: {}", film);
+                return film;
+            }
+        }
+
+        log.warn("Ошибка обновления фильма. Фильм с id {} не найден", film.getId());
+        throw new ValidationException("Фильм с id " + film.getId() + " не найден");
+    }
+
+    @GetMapping
+    public List<Film> getAllFilms() {
+        return films;
+    }
+
+    private void validate(Film film) {
+        if (film.getName() == null || film.getName().isBlank()) {
+            throw new ValidationException("Название фильма не может быть пустым");
+        }
+
+        if (film.getDescription() != null && film.getDescription().length() > 200) {
+            throw new ValidationException("Описание не может превышать 200 символов");
+        }
+
+        if (film.getDuration() <= 0) {
+            throw new ValidationException("Продолжительность должна быть положительным числом");
+        }
+    }
+
 }
