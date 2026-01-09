f849a7552fefeef4608e85d8c66841864ee3d52c
alonazrnko <alonazrnko@gmail.com>
2026-01-09 22:34:16 +0300

    fix checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -17,25 +17,39 @@ public class FilmController {
     private final FilmService filmService;
 
     @PostMapping
-    public Film create(@RequestBody @Valid FilmRequestDto dto) { return filmService.create(dto); }
+    public Film create(@RequestBody @Valid FilmRequestDto dto) {
+        return filmService.create(dto);
+    }
 
     @PutMapping
-    public Film update(@Valid @RequestBody Film film) { return filmService.update(film); }
+    public Film update(@Valid @RequestBody Film film) {
+        return filmService.update(film);
+    }
 
     @GetMapping
-    public Collection<Film> getAll() { return filmService.getAll(); }
+    public Collection<Film> getAll() {
+        return filmService.getAll();
+    }
 
     @GetMapping("/{id}")
-    public Film getById(@PathVariable long id) { return filmService.getById(id); }
+    public Film getById(@PathVariable long id) {
+        return filmService.getById(id);
+    }
 
     @PutMapping("/{id}/like/{userId}")
-    public void addLike(@PathVariable long id, @PathVariable long userId) { filmService.addLike(id, userId); }
+    public void addLike(@PathVariable long id, @PathVariable long userId) {
+        filmService.addLike(id, userId);
+    }
 
     @DeleteMapping("/{id}/like/{userId}")
-    public void removeLike(@PathVariable long id, @PathVariable long userId) { filmService.removeLike(id, userId); }
+    public void removeLike(@PathVariable long id, @PathVariable long userId) {
+        filmService.removeLike(id, userId);
+    }
 
     @GetMapping("/popular")
     public Collection<Film> getPopularFilms(
-            @RequestParam(defaultValue = "10") int count) { return filmService.getPopularFilms(count); }
+            @RequestParam(defaultValue = "10") int count) {
+        return filmService.getPopularFilms(count);
+    }
 }
 
