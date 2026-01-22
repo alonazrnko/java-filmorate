3eb23e952cfd41e1dd8962332da4ec088c211730
alonazrnko <alonazrnko@gmail.com>
2026-01-22 14:03:43 +0300

    refactor: replace MpaDbStorage with MpaRepository

--- src/main/java/ru/yandex/practicum/filmorate/dao/repository/MpaRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/repository/MpaRepository.java
@@ -1,4 +1,42 @@
 package ru.yandex.practicum.filmorate.dao.repository;
 
-public class MpaRepository {
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+import ru.yandex.practicum.filmorate.dao.repository.mappers.MpaRowMapper;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.MpaRating;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class MpaRepository extends BaseRepository<MpaRating> {
+    private static final String FIND_ALL_SQL = "SELECT * FROM mpa_rating";
+    private static final String FIND_BY_ID_SQL = "SELECT * FROM mpa_rating WHERE rating_id = ?";
+    private static final String FIND_ALL_BY_ID_SQL = "SELECT id, name FROM mpa_ratings WHERE id = ?";
+
+    public MpaRepository(JdbcTemplate jdbc, MpaRowMapper mapper) {
+        super(jdbc, mapper);
+    }
+
+    public Optional<MpaRating> findById(Long id) {
+        return findOne(FIND_BY_ID_SQL, id);
+    }
+
+    public List<MpaRating> findAll() {
+        return findMany(FIND_ALL_SQL);
+    }
+
+    private void loadMpa(Film film) {
+        if (film.getMpa() == null) {
+            return;
+        }
+
+        List<MpaRating> list = jdbc.query(FIND_ALL_BY_ID_SQL, mapper, film.getMpa().getId());
+
+        if (!list.isEmpty()) {
+            film.setMpa(list.get(0));
+        }
+    }
 }
