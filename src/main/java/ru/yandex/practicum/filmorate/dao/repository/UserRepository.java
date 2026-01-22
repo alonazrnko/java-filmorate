0fab422720cd78f28f338aeead18411d610b361d
alonazrnko <alonazrnko@gmail.com>
2026-01-22 14:05:01 +0300

    refactor: replace UserDbStorage with UserRepository

--- src/main/java/ru/yandex/practicum/filmorate/dao/repository/UserRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/repository/UserRepository.java
@@ -1,4 +1,52 @@
 package ru.yandex.practicum.filmorate.dao.repository;
 
-public class UserRepository {
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public class UserRepository extends BaseRepository<User> {
+    private static final String FIND_ALL_QUERY = "SELECT * FROM users";
+    private static final String FIND_BY_ID_QUERY = "SELECT * FROM users WHERE user_id = ?";
+    private static final String INSERT_QUERY = "INSERT INTO users(email, login, name, birthday) VALUES (?, ?, ?, ?)";
+    private static final String UPDATE_QUERY = "UPDATE users SET email = ?, login = ?, name = ?, birthday = ? WHERE user_id = ?";
+
+    public UserRepository(JdbcTemplate jdbc, RowMapper<User> mapper) {
+        super(jdbc, mapper);
+    }
+
+    public User create(User user) {
+        long id = insert(
+                INSERT_QUERY,
+                user.getEmail(),
+                user.getLogin(),
+                user.getName(),
+                user.getBirthday()
+        );
+        user.setId(id);
+        return user;
+    }
+
+    public User update(User user) {
+        update(
+                UPDATE_QUERY,
+                user.getEmail(),
+                user.getLogin(),
+                user.getName(),
+                user.getBirthday(),
+                user.getId()
+        );
+        return user;
+    }
+
+    public Collection<User> getAll() {
+        return findMany(FIND_ALL_QUERY);
+    }
+
+    public Optional<User> getById(long id) {
+        return findOne(FIND_BY_ID_QUERY, id);
+    }
 }
+
