5851e519e153175295a6a24f4dc103e4355702a9
alonazrnko <alonazrnko@gmail.com>
2026-01-22 12:36:28 +0300

    refactor: replace FriendshipStorage with FriendRepository

--- src/main/java/ru/yandex/practicum/filmorate/dao/repository/FriendRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/repository/FriendRepository.java
@@ -1,4 +1,41 @@
 package ru.yandex.practicum.filmorate.dao.repository;
 
-public class FriendRepository {
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+import ru.yandex.practicum.filmorate.model.Friendship;
+
+import java.util.List;
+
+@Repository
+public class FriendRepository extends BaseRepository<Friendship> {
+    private static final String FIND_BY_USER_ID_SQL = "SELECT * FROM friends WHERE user_id = ?";
+    private static final String INSERT_SQL = "INSERT INTO friends (user_id, friend_id, status) VALUES (?, ?, ?)";
+    private static final String DELETE_BY_IDS_SQL = "DELETE FROM friends WHERE user_id = ? AND friend_id = ?";
+    private static final String EXISTS_SQL = "SELECT COUNT(*) FROM friends WHERE user_id = ? AND friend_id = ?";
+
+    public FriendRepository(JdbcTemplate jdbc, RowMapper<Friendship> mapper) {
+        super(jdbc, mapper);
+    }
+
+    public void save(long userId, long friendId) {
+        if (exists(userId, friendId)) {
+            return;
+        }
+
+        jdbc.update(INSERT_SQL, userId, friendId);
+    }
+
+    public boolean delete(Long userId, Long friendId) {
+        return delete(DELETE_BY_IDS_SQL, userId, friendId);
+    }
+
+    public List<Friendship> findAllByUserId(Long userId) {
+        return findMany(FIND_BY_USER_ID_SQL, userId);
+    }
+
+    public boolean exists(Long userId, Long friendId) {
+        Integer count = jdbc.queryForObject(EXISTS_SQL, Integer.class, userId, friendId);
+        return count != null && count > 0;
+    }
 }
