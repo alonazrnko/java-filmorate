9446ef437641ab1d9d615e803fb4e387afd0b8da
alonazrnko <alonazrnko@gmail.com>
2026-01-22 11:51:42 +0300

    refactor: replace FilmDbStorage with FilmRepository

--- src/main/java/ru/yandex/practicum/filmorate/dao/repository/FilmRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/repository/FilmRepository.java
@@ -1,4 +1,56 @@
 package ru.yandex.practicum.filmorate.dao.repository;
 
-public class FilmRepository {
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.*;
+
+@Repository
+public class FilmRepository extends BaseRepository<Film> {
+    private static final String FIND_ALL_QUERY = "SELECT * FROM films";
+    private static final String FIND_BY_ID_QUERY = "SELECT * FROM films WHERE film_id = ?";
+    private static final String INSERT_QUERY = "INSERT INTO films(name, description, release_date, duration, rating_id)" +
+            "VALUES (?, ?, ?, ?, ?)";
+    private static final String UPDATE_QUERY = "UPDATE films SET name = ?, description = ?, release_date = ?, duration = ?, rating_id = ? WHERE film_id = ?";
+
+    public FilmRepository(JdbcTemplate jdbc, RowMapper<Film> mapper) {
+        super(jdbc, mapper);
+    }
+
+    public Film create(Film film) {
+        long id = insert(
+                INSERT_QUERY,
+                film.getName(),
+                film.getDescription(),
+                film.getReleaseDate(),
+                film.getDuration(),
+                film.getMpa()
+        );
+        film.setId(id);
+        return film;
+    }
+
+    public Film update(Film film) {
+        update(
+                UPDATE_QUERY,
+                film.getName(),
+                film.getDescription(),
+                film.getReleaseDate(),
+                film.getDuration(),
+                film.getMpa(),
+                film.getId()
+        );
+        return film;
+    }
+
+    public Collection<Film> getAll() {
+        return findMany(FIND_ALL_QUERY);
+    }
+
+
+    public Optional<Film> getById(Long id) {
+        return findOne(FIND_BY_ID_QUERY, id);
+    }
 }
